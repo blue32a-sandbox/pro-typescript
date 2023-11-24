7bfb88b44b80425e324abc3a7cfc166c06fc5324
blue32a <blue32a.jp@gmail.com>
2023-11-24 09:19:47 +0000

    2.1 式と文

---
+++ src/ch02/sentence-and-expression.ts
@@ -0,0 +1,13 @@
+// const 変数 = 式; という文
+const greeting = "Hello, ";
+const target = "world!";
+
+// 式 + 式を実行する式文
+console.log(greeting + target);
+
+const i = 5;
+
+// ３行全体が１つのif文
+if (i < 10) {
+  console.log("iは10未満です"); // これも１つの文
+}
